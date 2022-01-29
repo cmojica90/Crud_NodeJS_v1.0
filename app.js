a47da2867bdc4ac193af598170d1c072fa55ad42
cmojica90 <73602307+cmojica90@users.noreply.github.com>
2022-01-29 15:18:47 -0500

    Crud_V2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const app = express();
 
 //VAMOS A DEFINIR UN MOTOR DE PLANTILLA EN ESTE CASO VAMOS A TRABAJAR EL MOTOR DE PLANTILLA
 app.set('view engine', 'ejs');
 //VAMOS A DEFINIR UN MOTOR DE PLANTILLA EN ESTE CASO VAMOS A TRABAJAR EL MOTOR DE PLANTILLA
 //traemos el enrutador del archivo router.js
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use('/', require('./router'));
 
 /* app.get("/", (req, res) =>{
--- router.js
+++ router.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { send } = require("express/lib/response");
 const router = express.Router();
 
 const con = require('./database/db');
@@ -11,11 +10,41 @@ router.get('/', (req, res)=>{
        if (error){
             throw (error);
        }else{
-            res.send(results);
+            res.render('index', { results: results });//con el instanciador render vamos a visualizar los datos en nuestra plantilla
        }
    });
 })
+//ruta para crear un usuario
+router.get('/create', (req, res) => {
+     res.render('create');
+})
+//RUTA PARA ELIMINAR UN USUARIO
+router.get('/delete/:id', (req, res) => {
+     const id = req.params.id;
+     con.query('delete from users where id=?', [id], (error, results) => {
+          if (error) {
+               throw (error);
+          } else {
+               res.redirect('/');//con el instanciador render vamos a visualizar los datos en nuestra plantilla
+          }
+     })
+})
 
+//ruta para editar un usuario
+router.get('/edit/:id', (req, res) => {
+     const id = req.params.id;
+     con.query('select * from users where id=?', [id], (error, results) => {
+          if (error) {
+               throw (error);
+          } else {
+               res.render('edit', { results: results[0] });//con el instanciador render vamos a visualizar los datos en nuestra plantilla
+          }
+     })
+})
+const crud = require('./controllers/crud'); // Con esto mandamos a llamar al metodo save
+const { append } = require("express/lib/response");
+router.post('/save', crud.save);
+router.post('/update', crud.update);
 module.exports = router;
 
 
